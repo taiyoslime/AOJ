b21429fd02b417a409b205be329091faa60da1ea
taiyoslime <taiyoslime@taiyoslime-MacBook-Pro.local>
2015-11-06 18:14:56 +0900

    test

--- ALDS1_1_A.cpp
+++ ALDS1_1_A.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main()
 {
-  int n,num[3000];
+  int j,v,n;
   cin>>n;
+  int num[n];
   for(int i=0;i<n;i++)
   {
-      vector <int>
-  }for (size_t i = 0; i < count; i++) {
+    cin >> num[i];
+  }
+  for (int i = 1; i < n; i++) {
+      v = num[i];
+      j = i - 1;
+      while(j >= 0 && num[j] > v)
+      {
+      num[j+1] = num[j];
+      j--;
+      }
+  num[j+1] = v;
+
+  for(int i = 0;i < n-1;i++)
+  {
+    cout<<num[i]<<" ";
+  }
+  cout << num[n-1] << endl;
+
   }
 
 }
