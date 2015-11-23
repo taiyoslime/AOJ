6ffabea08692b587ca920bfa3c1abadfb91eb843
taiyoslime <hase.tekitou@gmail.com>
2015-11-23 22:30:09 +0900

    バグ見つからないので寝ます

--- 0525.cpp
+++ 0525.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 using namespace std;
-int a,b,ans = -111111,map[10003][13]={0},table[10003] = {0};
+
+int a,b,ans = -1,map[10003][13],table[10003];
 int rmax(int _array[]){
       int ptable[13],ans = 0,count;
+
       for(int i = 0;i < b;i++){
             count = 0;
             for(int j= 0;j<a;j++){
-                  if(_array[j] == 0)
+                  if(_array[j] == 1)
                         ptable[j] = map[j][i];
-                  else{
-                        if(map[j][i] == 1)ptable[j] = 0;
-                        else ptable[j] = 1;
-                  }
-                  if(ptable[j] == 1) count++;
+                  else
+                        ptable[j] = map[j][i]^1;
+                  if(ptable[j]) count++;
             }
-            ans += max(count , a  - count);
+            ans += max(count , a-count);
       }
       cout << ans << endl;
       return ans;
 }
+
 void dfs(int array[],int num){
       if(num == a){
             ans = max(ans,rmax(array));
@@ -30,14 +31,16 @@ void dfs(int array[],int num){
       dfs(array,num+1);
 }
 int main(){
-      while(cin){
+      while(1){
+
             cin >> a >> b;
+            if(!a&&!b)return 0;
             for(int i = 0 ; i < a;i++){
                   for(int j = 0;j<b;j++)
                         cin >> map[i][j];
             }
       dfs(table,0);
+      cout <<endl;
       cout << ans << endl;
       }
-
 }
