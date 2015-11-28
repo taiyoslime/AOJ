374dd51e1a5a4d7e93598cf8d319b9a28690dac8
taiyoslime <hase.tekitou@gmail.com>
2015-11-28 20:12:31 +0900

    うああああ

--- 0578.cpp
+++ 0578.cpp
@@ -1,7 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
+
+string str,tmp;
+
+int judge(int num,string jud)
+{
+      if(jud.find(str) != string::npos)
+            return 1;
+      else
+      {
+            if(num == tmp.length()-1)
+                  return 0;
+            else
+            {
+                  string f;
+                  for(int i = 0; i < num ; i++)
+                  {
+                        f +=
+
+                  }
+            }
+      }
+
+}
+
 int main()
 {
+      int x,ctn = 0;
 
+      cin >> x >> str;
+      for (int i = 0;i < x ; i++)
+      {
+            cin >> tmp;
+            ctn += judge(0,tmp);
+      }
+      cout << ctn << endl;
       return 0;
 }
