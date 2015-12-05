adb595cd43c4625a809f82a48d38c8f7eba8058d
taiyoslime <hase.tekitou@gmail.com>
2015-12-06 01:26:37 +0900

    revert

--- DPL1_C.cpp
+++ DPL1_C.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
-int
+int dp[10005];
 int main(){
-
-
+      int n,sum,vs[1005],ws[1005];cin >> n >> sum;
+      for (int i = 0;i<n;i++)cin >> vs[i] >> ws[i];
+      for (int i = 0;i <= sum;i++)for(int j = 0;j <n;j++)if(i + ws[j] <=sum ) dp[i + ws[j]] = max(dp[i]+vs[j],dp[i+ws[j]]);
+      cout << dp[sum] << endl;
 }
