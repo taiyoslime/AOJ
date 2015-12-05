64672ed00457aeba41d322c9cce73d7eecd881fd
taiyoslime <hase.tekitou@gmail.com>
2015-12-05 19:31:20 +0900

    test

--- DPL1_C.cpp
+++ DPL1_C.cpp
@@ -6,4 +6,3 @@ int main(){
       for (int i = 0;i<n;i++)cin >> vs[i] >> ws[i];
       for (int i = 0;i <= sum;i++)for(int j = 0;j <n;j++)if(i + ws[j] <=sum ) dp[i + ws[j]] = max(dp[i]+vs[j],dp[i+ws[j]]);
       cout << dp[sum] << endl;
-}
