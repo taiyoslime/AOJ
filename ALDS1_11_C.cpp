097bb3ea4b5b1b2d9b2a5024991984b5ac1fd842
taiyoslime <hase.tekitou@gmail.com>
2015-12-09 22:33:54 +0900

    add and change files\(no comment\)

--- ALDS1_11_C.cpp
+++ ALDS1_11_C.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 int d[128][128],solve[128];
 int main(){
-      //input
       int n,m,row,t;
       queue<int> que;
       cin >> n;
@@ -15,29 +14,18 @@ int main(){
                   d[row][t] = 1;
             }
       }
-      //debug
-      //for(int i = 0;i<=n;i++){
-      //for(int j = 0;j<=n;j++)cout << d[i][j] << ' ';cout << endl;}
-
-      //solve
       fill(solve,solve+n+2,-1);
       que.push(1);
       solve[1] = 0;
-      while(!que.empty())
-      {
+      while(!que.empty()){
             int p = que.front();
             que.pop();
-            for(int i = 1;i<=n;i++)
-            {
-                  if(d[p][i] && solve[i]==-1)
-                  {
+            for(int i = 1;i<=n;i++){
+                  if(d[p][i] && solve[i]==-1){
                         que.push(i);
                         solve[i] = solve[p] + 1;
                   }
             }
       }
-      for(int i = 1;i<=n;i++)
-            cout << i << ' '<< solve[i]<<endl;
-
-
+      for(int i = 1;i<=n;i++)cout << i << ' '<< solve[i]<<endl;
 }
