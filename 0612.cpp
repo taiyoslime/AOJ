7f5cb768e8d693e4e1aada0ff62db667de30c20c
taiyoslime <hase.tekitou@gmail.com>
2015-12-11 18:11:58 +0900

    mapで実装

--- 0612.cpp
+++ 0612.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
+#include <map>
 #define FLAG -1
 using namespace std;
 typedef pair<int,int> P;
 int main(){
       int h,w,dh[] = {1,1,1,0,-1,-1,-1,0}, dw[] = {1,0,-1,-1,-1,0,1,1},ctn,i,j;
-      int solve = -1 ;
+      int solve = -2 ;
       cin >> h >> w;
       queue<P> que,tmp;
       char d[h][w];
@@ -18,18 +19,31 @@ int main(){
                         que.push(make_pair(i,j));
             }
       }
+
       que.push(make_pair(FLAG,FLAG));
       for(;;){
             solve++;
+            map<P,bool> m;
             for(;;){
-                  P p = que.front();que.pop();
-                  if(p.first==FLAG)break;
-                  ctn =0;
-                  for(i = 0;i < 8;i++)
+                  //cout << que.size()<<endl;
+                  P p = que.front();
+                  que.pop();
+                  if(p.first==FLAG)
+                  {//cout <<" break "<<endl<<endl;
+                  break;
+                  }
+                  ctn = 0;
+                  for(i = 0;i < 8;i++){
                         if(d[p.first+dh[i]][p.second+dw[i]]=='.')ctn++;
-                  if(ctn>=(int)(d[p.first][p.second]-'0')){
+                  }
+
+                  if(ctn >= d[p.first][p.second]-'0'){
                         tmp.push(p);
-                        for(j=0;j<8;j++)que.push(make_pair(p.first+dh[j],p.second+dw[j]));
+                        for(j=0;j<8;j++)
+                        if(m.count(make_pair(p.first+dh[j],p.second+dw[j]))==0&&d[p.first][p.second]!='.'){
+                        que.push(make_pair(p.first+dh[j],p.second+dw[j]));
+                        m[make_pair(p.first+dh[j],p.second+dw[j])]=true;
+                        }
                   }
             }
             if(tmp.empty())break;
@@ -38,6 +52,15 @@ int main(){
                   d[tmp.front().first][tmp.front().second]='.';
                   tmp.pop();
             }
+            /*for(i =0 ;i<h;i++)
+            {
+                  for(j=0;j<w;j++)cout << d[i][j];
+                  cout <<endl;
+            }
+            */
+
+            que.push(make_pair(FLAG,FLAG));
+
       }
       cout << solve << endl;
 }
