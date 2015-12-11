b699fc4198257c6bf67d40c2e3f046c93c9096e4
taiyoslime <hase.tekitou@gmail.com>
2015-12-11 22:48:28 +0900

    git puls test

--- 0561.cpp
+++ 0561.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
+vector<int> books[10];
 int main(){
-      int n,k;
+      int n,k,i,j,tm1,tm2;
       cin >> n >> k;
-      
+      for(i=0;i<n;i++){
+            cin >> tm1>>tm2;
+            books[tm2].push_back(tm1);
+      }
+      for(i=0;i<10;i++)sort(books[i].begin(),books[i].end());
+
+      int dp[10][2005];
+      for(i=0;i<10;i++){
+            for(j=0;j<=k;j++){
+
+
+
+            }
+
+      }
+
 }
