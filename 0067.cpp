edf9625e408b9a497204f1b5d2f3a05f05f7fc15
taiyoslime <hase.tekitou@gmail.com>
2015-12-11 21:30:09 +0900

    たのちい

--- 0067.cpp
+++ 0067.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
-typedef pair<int,int> P;
-vector<string> ch;
+char ch[12][12];
 int dh[]={1,0,-1,0},dw[]={0,-1,0,1};
-void dfs(int h,int w)
-{
-      for(int i=0;i<4;i++)
-      {
+void dfs(int h,int w){
+      ch[h][w]='0';
+      for(int i=0;i<4;i++){
             int nx=h+dh[i],ny=w+dw[i];
             if(nx>=0&&nx<12&&ny>=0&&ny<12&&ch[nx][ny]=='1')dfs(nx,ny);
       }
-      return;
+      return ;
 }
 int main(){
-
-      while(1){
-            int solve=0,tmp;
-            for(int i=0;i<12;i++){cin>>tmp;ch.push_back(tmp);}
+      while(cin>>ch[0]){
+            int solve=0;
+            for(int i=1;i<12;i++)cin>>ch[i];
             for(int i=0;i<12;i++){
                   for(int j= 0;j<12;j++)
                   if(ch[i][j]=='1'){dfs(i,j);solve++;}
