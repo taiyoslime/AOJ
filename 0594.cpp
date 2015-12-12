3b8f95eafb16b5fedf644f5b44eb6d50b5123996
taiyoslime <hase.tekitou@gmail.com>
2015-12-12 15:52:56 +0900

    0594綺麗にかけて嬉しい

--- 0594.cpp
+++ 0594.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 int main(){
-      int w,h,n,fx,fy,tx,ty,solve=0;
-      cin>>w>>h>>n;
-      cin>>tx>>ty;
-      for(int i=1;i<n;i++){
+      int w,n,fx,fy,tx,ty,solve=0,i=1;
+      cin>>w>>w>>n>>tx>>ty;
+      for(;i<n;i++){
             fx=tx,fy=ty;
             cin>>tx>>ty;
-            if((tx-fx>0&&ty-fy>0)||(fx-tx>0&&fy-ty>0))solve+=max(abs(tx-fx),abs(ty-fy));
-            else solve+= (abs(tx-fx)+abs(ty-fy));
+            solve+=((tx-fx)*(ty-fy)>0)?max(abs(tx-fx),abs(ty-fy)):(abs(tx-fx)+abs(ty-fy));
       }
       cout<<solve<<endl;
 }
