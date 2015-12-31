88a004d1958365ea1998030a49a1957b7c3f073a
taiyoslime <hase.tekitou@gmail.com>
2015-12-31 16:22:59 +0900

    add and change files(no comment)

--- 0502.cpp
+++ 0502.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 int main(){
       int d[6]={1,5,3,2,4,6}; //
-      int top,front,ctn = 0,tmp;
+      int ctn=1,tmp;
       string str;
       cin>>tmp;
+      if(!tmp)return 0;
       for(int i=0;i<tmp;i++){
             cin >> str;
             switch (str[0]) {
@@ -24,13 +25,23 @@ int main(){
                         swap(d[5],d[4]);
                         swap(d[4],d[0]);
                         break;
-                  case 'S': 
+                  case 'S':
+                        swap(d[0],d[1]);
+                        swap(d[1],d[5]);
+                        swap(d[5],d[3]);
                         break;
                   case 'R':
+                        swap(d[1],d[2]);
+                        swap(d[1],d[4]);
+                        swap(d[3],d[4]);
                         break;
                   case 'L':
+                        swap(d[3],d[4]);
+                        swap(d[4],d[1]);
+                        swap(d[1],d[2]);
                         break;
             }
+            ctn+=d[0];
       }
-
+      cout <<ctn <<endl;
 }
