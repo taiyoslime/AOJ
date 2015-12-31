a6653387460a54c7c9e55320c6445c7425cc9896
taiyoslime <hase.tekitou@gmail.com>
2015-12-31 16:41:42 +0900

    add and change files(no comment)

--- 0502.cpp
+++ 0502.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <string>
+#define sw(w,x,y,z) swap(d[w],d[x]);swap(d[x],d[y]);swap(d[y],d[z]);break;
 using namespace std;
 int main(){
       string str;
       int tmp;
       while(cin>>tmp&&tmp){
-      int d[]={1,5,3,2,4,6},ctn=1;
+            int d[]={1,5,3,2,4,6},ctn=1;
             for(int i=0;i<tmp;i++){
                   cin>>str;
                   switch (str[0]) {
-                        case 'N':swap(d[0],d[3]);swap(d[3],d[5]);swap(d[5],d[1]);break;
-                        case 'E':swap(d[0],d[4]);swap(d[4],d[5]);swap(d[5],d[2]);break;
-                        case 'W':swap(d[2],d[5]);swap(d[5],d[4]);swap(d[4],d[0]);break;
-                        case 'S':swap(d[0],d[1]);swap(d[1],d[5]);swap(d[5],d[3]);break;
-                        case 'R':swap(d[1],d[2]);swap(d[1],d[4]);swap(d[3],d[4]);break;
-                        case 'L':swap(d[3],d[4]);swap(d[4],d[1]);swap(d[1],d[2]);break;
+                        case 'N':sw(0,3,5,1);
+                        case 'E':sw(0,4,5,2);
+                        case 'W':sw(2,5,4,0);
+                        case 'S':sw(0,1,5,3);
+                        case 'R':sw(2,1,4,3);
+                        case 'L':sw(3,4,1,2);
                   }
                   ctn+=d[0];
             }
