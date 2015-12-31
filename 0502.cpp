b8bbbcde5bb88e4f08fa21a5bd0202c74b099982
taiyoslime <hase.tekitou@gmail.com>
2015-12-31 10:10:03 +0900

    add and change files(no comment)

--- 0502.cpp
+++ 0502.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(){
       int i[6]={1,6,2,5,3,4};
-      
+      int top,front,ctn = 0,tmp;
+      string str;
+      cin>>tmp;
+      for(int i=0;i<tmp;i++){
+            cin >> str;
+            switch (str[0]) {
+                  case "N":
+                        break;
+                  case "E":
+                        break;
+                  
+
+
+
+
+
+
+            }
+      }
+
 }
