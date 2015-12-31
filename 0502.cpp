f98157e7ef3a79ce9b5b442703d05cc2e309607f
taiyoslime <hase.tekitou@gmail.com>
2015-12-31 16:36:40 +0900

    add and change files(no comment)

--- 0502.cpp
+++ 0502.cpp
@@ -9,39 +9,15 @@ int main(){
             for(int i=0;i<tmp;i++){
                   cin>>str;
                   switch (str[0]) {
-                        case 'N':
-                              swap(d[0],d[3]);
-                        swap(d[3],d[5]);
-                              swap(d[5],d[1]);
-                              break;
-                        case 'E':
-                              swap(d[0],d[4]);
-                              swap(d[4],d[5]);
-                              swap(d[5],d[2]);
-                              break;
-                        case 'W':
-                              swap(d[2],d[5]);
-                              swap(d[5],d[4]);
-                              swap(d[4],d[0]);
-                              break;
-                              case 'S':
-                              swap(d[0],d[1]);
-                              swap(d[1],d[5]);
-                              swap(d[5],d[3]);
-                              break;
-                        case 'R':
-                              swap(d[1],d[2]);
-                              swap(d[1],d[4]);
-                              swap(d[3],d[4]);
-                              break;
-                        case 'L':
-                              swap(d[3],d[4]);
-                              swap(d[4],d[1]);
-                              swap(d[1],d[2]);
-                              break;
-                        }
-                        ctn+=d[0];
+                        case 'N':swap(d[0],d[3]);swap(d[3],d[5]);swap(d[5],d[1]);break;
+                        case 'E':swap(d[0],d[4]);swap(d[4],d[5]);swap(d[5],d[2]);break;
+                        case 'W':swap(d[2],d[5]);swap(d[5],d[4]);swap(d[4],d[0]);break;
+                        case 'S':swap(d[0],d[1]);swap(d[1],d[5]);swap(d[5],d[3]);break;
+                        case 'R':swap(d[1],d[2]);swap(d[1],d[4]);swap(d[3],d[4]);break;
+                        case 'L':swap(d[3],d[4]);swap(d[4],d[1]);swap(d[1],d[2]);break;
                   }
-                  cout <<ctn <<endl;
-            }          
+                  ctn+=d[0];
+            }
+            cout <<ctn <<endl;
+      }
 }
