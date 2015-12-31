c08ba181f09c55fce907f3f506c81586e869097a
taiyoslime <hase.tekitou@gmail.com>
2015-12-31 16:49:56 +0900

    add and change files(no comment)

--- 0502.cpp
+++ 0502.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#define sw(w,x,y,z) swap(d[w],d[x]);swap(d[x],d[y]);swap(d[y],d[z]);break;
+#define s(w,x,y,z) swap(d[w],d[x]);swap(d[x],d[y]);swap(d[y],d[z]);break;
 using namespace std;
 int main(){
       string str;int tmp,i;
@@ -9,12 +9,12 @@ int main(){
             for(i=0;i<tmp;i++){
                   cin>>str;
                   switch(str[0]){
-                        case 'N':sw(0,3,5,1);
-                        case 'E':sw(0,4,5,2);
-                        case 'W':sw(2,5,4,0);
-                        case 'S':sw(0,1,5,3);
-                        case 'R':sw(2,1,4,3);
-                        case 'L':sw(3,4,1,2);
+                        case 'N':s(0,3,5,1);
+                        case 'E':s(0,4,5,2);
+                        case 'W':s(2,5,4,0);
+                        case 'S':s(0,1,5,3);
+                        case 'R':s(2,1,4,3);
+                        case 'L':s(3,4,1,2);
                   }
                   ctn+=d[0];
             }
