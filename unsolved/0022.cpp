5a28eeaaebc121b3a899aa51774e4886be6190bd
taiyoslime <hase.tekitou@gmail.com>
2016-01-04 14:21:41 +0900

    add and change files(no comment)

---
+++ unsolved/0022.cpp
@@ -0,0 +1,12 @@
+//未提出
+
+#include <iostream>
+using namespace std;
+int main(){
+    long n,sum,t,i;
+    while(cin>>n&&n){
+        sum=0;
+        for(i=0;i<n;i++){cin>>t;sum+=t;}
+        cout<<sum<<endl;
+    }
+}
