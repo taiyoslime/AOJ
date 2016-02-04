71037e3d7a41ba1bc17789e6afa1b9968641c2fd
Taiyo Mizuhashi <contact.taiyoslime@gmail.com>
2016-02-04 23:05:05 +0900

    Update practice_dp.cpp

--- archves/practice_dp.cpp
+++ archves/practice_dp.cpp
@@ -7,17 +7,17 @@ int main(){
       int ws[5] = { 3,4,1,2,3 };
       int ps[5] = { 2,3,2,3,6 };
 
-      for (int i = 0; i <= 5; i++){
-            for(int j = 0; j <= maxw; j++){
+      for (int i=0;i<=5;i++){
+            for(int j=0;j<=maxw;j++){
                   if(j + ws[i] <= maxw){
                         dp[i+1][j+ws[i]] = max(dp[i+1][j+ws[i]],dp[i][j] + ps[i]);
                         ret = max(dp[i+1][j+ws[i]],ret);
                   }
             }
       }
-      for(int i= 0; i< 6;i++){
-            for(int j=0; j<11;j++)cout << dp[i][j]<<' ';
-                  cout << endl;
+      for(int i= 0;i<6;i++){
+            for(int j=0;j<11;j++)cout<<dp[i][j]<<' ';
+                  cout<<endl;
       }
       cout << ret << endl;
 }
