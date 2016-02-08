83a3d424a2bb8ac420b629cc3091e16310dbf903
taiyoslime <hase.tekitou@gmail.com>
2016-02-08 19:56:27 +0900

    rewrite 0005.cpp

--- 0005.cpp
+++ 0005.cpp
@@ -2,18 +2,10 @@
 using namespace std;
 typedef long long ll;
 ll gcd(ll m, ll n){
-      while(m!=n){
-  		if (m>n) m = m - n;
-  		else n = n - m;
-  	}
-  	return m;
-}
-ll lcm(ll m,ll n){
-      return ((m/gcd(m, n))*n);
+	while(m!=n)m>n?m-=n:n-=m;
+	return m;
 }
 int main(){
-      ll a,b;
-      while(cin>>a>>b){
-            cout<<gcd(a,b)<<' '<<lcm(a,b)<<endl;
-      }
+	ll a,b;
+	while(cin>>a>>b)cout<<gcd(a,b)<<' '<<a/gcd(a,b)*b<<endl;
 }
