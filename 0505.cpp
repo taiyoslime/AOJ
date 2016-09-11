b9cf607274b465de89984526180d70d00983bdd5
taiyoslime <hase.tekitou@gmail.com>
2016-09-11 23:47:53 +0900

    QoL

--- 0505.cpp
+++ 0505.cpp
@@ -1,23 +1,12 @@
-#include <iostream>
-#include <cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
-	int n,m,ctn[1000],t;
+	int n,m,t;
 	while(cin>>n>>m,n){
-		fill(ctn,ctn+m,0);
-		for(int i=0;i<n;i++)for(int j=0;j<m;j++)cin>>t,ctn[j]+=t;
-		t=0;
-		int i=n;
-		while(t!=m-1){
-			for(int j=0;j<m;j++){
-				if(ctn[j]==i){
-					cout<<j+1<<" ";
-					t++;
-					ctn[j]=-1;
-				}
-			}
-			i--;
-		}
-		for(int i=0;i<m;i++)if(ctn[i]!=-1)cout<<i+1<<endl;
+		pair<int,int> v[m];
+		for(int i=0;i<m;i++)v[i]=make_pair(n,i+1);
+		for(int i=0;i<n;i++)for(int j=0;j<m;j++)cin>>t,v[j].first-=t;
+		sort(v,v+m);
+		for(int i=0;i<m-1;i++)cout<<v[i].second<<' ';cout<<v[m-1].second<<endl;
 	}
 }
--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 # 競プロ万歳
 
 個人的なバックアップのためにOnline Judgeやその他で通したソースコードをあげてます
+
+可読性が最悪なのでソース見るとQoLが低下します。
+
 ```
-./         : AOJ
-./unsolved : AOJ未AC
-./atcoder  : Atcoder
-./scaffold : 雛形（いらない）
-./archives : その他
+/         : AOJ
+/unsolved : AOJ未AC
+/atcoder  : Atcoder
+/scaffold : 雛形（いらない）
+/archives : その他
 ```
 ##### 実行環境
-- gcc 5.3.0 ( Homebrew gcc )
-- Mac OS X 10.10.5
+- gcc 5.3.0 (Homebrew gcc)
+- Mac OS X El Captain 10.11.5
+
+```
+$ g++ ${filename} -std=c++11 -Wall -O2
+```
 
 #### [個人的に得た知見 - node.md](./note.md)
