1b7ae71fd2564b6e950fb0698169d01680a3b132
taiyoslime <hase.tekitou@gmail.com>
2016-11-11 17:54:17 +0900

    add and change files(no comment)

--- atcoder/ABC/038_C.cpp
+++ atcoder/ABC/038_C.cpp
@@ -1,5 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-
+	int n,a,c=1,m;
+	long long r=1,s=0;
+	cin>>n>>m;
+	while((n--)-1){
+		cin>>a;
+		if(m<a)c++,r+=c;
+		else s+=r,c=1,r=1;
+		m=a;
+	}
+	s+=r;
+	cout<<s<<endl;
 }
