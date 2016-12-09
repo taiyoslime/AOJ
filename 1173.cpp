5f908ac1dfd89cc85eabb568f468ea5160af8099
taiyoslime <hase.tekitou@gmail.com>
2016-12-09 21:08:01 +0900

    gotoを書いたのでQoLが低い

--- 1173.cpp
+++ 1173.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+string s;
 int main(){
+	G:
+	while(getline(cin,s),s!="."){
+		stack<char> p;
+		for(int i=0;i<s.length();i++){
+			if(s[i]=='('||s[i]=='[')p.push(s[i]);
+			if(s[i]==')'){if(p.empty()||p.top()!='('){cout<<"no"<<endl;goto G;}p.pop();}
+			if(s[i]==']'){if(p.empty()||p.top()!='['){cout<<"no"<<endl;goto G;}p.pop();}
+		}
+		cout<<(p.empty()?"yes":"no")<<endl;
+	}
 
 }
