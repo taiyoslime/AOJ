f55ac65329011ae9d37ac4fcaae5528e49182a5a
taiyoslime <hase.tekitou@gmail.com>
2016-12-09 22:25:46 +0900

    add and change files(no comment)

--- 1130.cpp
+++ 1130.cpp
@@ -3,16 +3,16 @@ using namespace std;
 char maze[21][21];
 #define r(z,x) for(int z=0;z<x;z++)
 int ans,a,b;
-void dfs(int x,int y){
+void bfs(int x,int y){
 	int dx[4]={0,-1,0,1},dy[4]={1,0,-1,0};
 	maze[x][y]='#',ans++;
-	r(i,4)if(maze[x+dx[i]][y+dy[i]]=='.')dfs(x+dx[i],y+dy[i]);
+	r(i,4)if(maze[x+dx[i]][y+dy[i]]=='.')bfs(x+dx[i],y+dy[i]);
 }
 int main(){
 	while(cin>>a>>b,a||b){
 		ans=0;
 		fill(maze[0],maze[20],'#');
 		r(i,b)r(j,a)cin>>maze[i][j];
-		r(i,b)r(j,a)if(maze[i][j]=='@')dfs(i,j),cout<<ans<<endl;
+		r(i,b)r(j,a)if(maze[i][j]=='@')bfs(i,j),cout<<ans<<endl;
 	}
 }
